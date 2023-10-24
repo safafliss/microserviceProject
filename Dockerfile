7aa581804954789336faab56d759adaa22ab2fc3
SkanderGassa <skander.gassa@esprit.tn>
2023-10-24 16:47:33 +0100

    Entretiens

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:8
-EXPOSE 8088
-ADD target/formation-0.0.1-SNAPSHOT.jar formation.jar
-ENTRYPOINT ["java","-jar","formation.jar"]
+EXPOSE 8080
+ADD target/entretiens.jar entretiens.jar
+ENTRYPOINT ["java", "-jar", "entretiens.jar"]
--- src/main/java/tn/esprit/formation/repositories/FormationRepository.java
+++
@@ -1,8 +0,0 @@
-package tn.esprit.formation.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.formation.entities.Formation;
-
-public interface FormationRepository extends JpaRepository<Formation, Long> {
-
-}
